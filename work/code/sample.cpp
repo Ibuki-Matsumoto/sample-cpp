67454cb3552dc7aa6a17661041ea1ee1526a5222
Ibuki-Matsumoto <n4rvs3apex@gmail.com>
2022-05-17 09:40:14 +0900

    init commit

---
+++ work/code/sample.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+	int tosi;
+
+	cout << "hello, I'm computer." << endl;
+	cout << "please, tell your name." << endl;
+
+	cin >> name;
+
+	cout << "Hello," << name <<endl;
+
+	cout << "How Old are you?" << endl;
+
+	cin >> tosi;
+
+	cout << "ok," << tosi << endl;
+}
